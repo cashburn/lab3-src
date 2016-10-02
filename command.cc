c01d1b6b018deeff4327eb123703158328e656b3
Colin Ashburn <cashburn@purdue.edu>
2016-10-02 14:26:02 -0400

    Commit

--- command.cc
+++ command.cc
@@ -187,7 +187,7 @@ Command::execute()
     if (!strcmp(_simpleCommands[0]->_arguments[0], "cd")) {
         const char * temp;
         if (_simpleCommands[0]->_numOfArguments < 2) {
-            temp = getenv("PWD");
+            temp = getenv("HOME");
         }
         else
             temp = _simpleCommands[0]->_arguments[1];
