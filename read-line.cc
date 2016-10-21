e8cc7741bf4a985952ab8b851ce463b2bf721ced
Colin Ashburn <cashburn@purdue.edu>
2016-10-20 22:30:29 -0400

    Commit

--- read-line.cc
+++ read-line.cc
@@ -161,7 +161,7 @@ char * read_line() {
 	}	
 
 	// Copy line from history
-	strcpy(line_buffer, history[++history_index]);
+	strcpy(line_buffer, history[history_index++]);
 	line_length = strlen(line_buffer);
 
 	// echo line
