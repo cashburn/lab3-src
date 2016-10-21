f255eee3bd9693d1c648cba04145e6dda4fd4529
Colin Ashburn <cashburn@purdue.edu>
2016-10-20 22:46:29 -0400

    Commit

--- read-line.cc
+++ read-line.cc
@@ -102,7 +102,7 @@ char * read_line() {
       char ch2;
       read(0, &ch1, 1);
       read(0, &ch2, 1);
-      printf("%d\n", history_index);
+      //printf("%d\n", history_index);
       if (ch1==91 && ch2==65) {
 	// Up arrow. Print next line in history.
         if (history_index < 1)
